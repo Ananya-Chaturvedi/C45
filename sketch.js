d559d90443c2e63b6c8b41d73f03fe9c17c17bb0
Ananya-Chaturvedi <78731750+Ananya-Chaturvedi@users.noreply.github.com>
2021-07-30 18:23:32 +0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,93 @@
+var bg,bgImg;
+var player, shooterImg, shooter_shooting;
+var zombie,zombieImg,zombieGroup;
+var bullet;
+var score = 0;
+var dzImg;
+
+function preload(){
+  
+  shooterImg = loadImage("shooter_2.png")
+  shooter_shooting = loadImage("shooter_3.png")
+
+  bgImg = loadImage("bg.jpeg")
+
+  zombieImg = loadImage("zombie.png");
+
+  dzImg = loadImage("hand.png");
+
+
+}
+
+function setup() {
+  
+  createCanvas(windowWidth,windowHeight);
+
+  //adding the background image
+  bg = createSprite(displayWidth/2-20,displayHeight/2-40,20,20)
+bg.addImage(bgImg)
+bg.scale = 1.1
+  
+
+//creating the player sprite
+player = createSprite(displayWidth-1150, displayHeight-300, 50, 50);
+ player.addImage(shooterImg)
+   player.scale = 0.3
+   player.debug = true
+   player.setCollider("rectangle",0,0,300,300)
+
+zombieGroup = new Group();
+
+bullet = createSprite(displayWidth-1000,displayHeight-300,10,5);
+bullet.shapeColor = "white";
+
+
+}
+
+function draw() {
+  background(0); 
+
+  //moving the player up and down and making the game mobile compatible using touches
+if(keyDown("UP_ARROW")||touches.length>0){
+  player.y = player.y-30
+}
+if(keyDown("DOWN_ARROW")||touches.length>0){
+ player.y = player.y+30
+}
+
+//release bullets and change the image of shooter to shooting position when space is pressed
+if(keyWentDown("space")){
+ 
+  player.addImage(shooter_shooting)
+ 
+}
+
+//player goes back to original standing image once we stop pressing the space bar
+else if(keyWentUp("space")){
+  player.addImage(shooterImg)
+  bullet.velocityX = 3;
+}
+spawnZombies();
+
+if(bullet.isTouching(zombie)){
+bullet.destroy();
+zombie.addImage(dzImg);
+zombie.scale = 0.08;
+score = score+1;
+}
+fill("white");
+textSize(20);
+text("Score: "+ score,displayWidth-300,displayHeight-500);
+
+drawSprites();
+
+}
+
+function spawnZombies(){
+zombie = createSprite(displayWidth-500,displayHeight-300,20,20);
+zombie.addImage(zombieImg);
+zombie.scale = 0.2;
+zombie.setCollider("rectangle",0,0,300,300)
+zombie.debug = true;
+zombieGroup.add(zombie);
+}
